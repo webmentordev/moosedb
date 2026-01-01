ffad9f75d3ffacf2af7fbcdcdfdb5b31bc013e90
webmentordev <ahmertahir99@gmail.com>
2026-01-01 20:41:52 +0500

    CLI Command to update super admin password

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -2,3 +2,4 @@ mod connection;
 
 pub use connection::initialize_db;
 pub use connection::load_configs;
+pub use connection::update_super_user;
--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,4 @@ pub mod utils;
 
 pub use db::initialize_db;
 pub use db::load_configs;
+pub use db::update_super_user;
