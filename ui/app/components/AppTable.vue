0b46e237832fd1b4efd53576d853a04ec1d0b128
webmentordev <ahmertahir99@gmail.com>
2026-02-11 19:14:04 +0500

    Table UI

--- ui/app/components/AppTable.vue
+++ ui/app/components/AppTable.vue
@@ -1,17 +1,17 @@
 <template>
-    <table class="w-full h-fit border-collapse">
+    <table class="w-full border-collapse">
         <thead>
             <tr class="border-b border-para">
                 <th v-for="column in columns" :key="column.name"
-                    class="px-6 py-4 text-left text-sm font-semibold text-main uppercase tracking-wider bg-light">
+                    class="px-6 py-3.5 text-left text-xs font-medium text-para-light tracking-wide">
                     {{ column.name }}
                 </th>
             </tr>
         </thead>
         <tbody>
             <tr v-for="(record, index) in records" :key="index"
-                class="border-b border-para hover:bg-light transition-colors duration-150">
-                <td v-for="column in columns" :key="column.name" class="px-6 py-4 text-sm text-gray-300">
+                class="border-b border-para/50 hover:bg-light/50 transition-colors">
+                <td v-for="column in columns" :key="column.name" class="px-6 py-3.5 text-sm text-para-light">
                     {{ record[column.name] }}
                 </td>
             </tr>
