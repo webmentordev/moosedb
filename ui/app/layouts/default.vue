e1814a4f78950d07aedf2c73de9bd131750152e3
webmentordev <ahmertahir99@gmail.com>
2026-02-28 13:53:09 +0500

    Usin app name

--- ui/app/layouts/default.vue
+++ ui/app/layouts/default.vue
@@ -7,3 +7,27 @@
     </div>
   </section>
 </template>
+
+<script setup lang="js">
+const appname = ref("MooseDB");
+const { authFetch } = useAuthFetch();
+try {
+  const data = await authFetch('/admin/api/get-setting', {
+    method: "POST",
+    body: {
+      key: "appname"
+    }
+  });
+  if (data.success == true) {
+    appname.value = data.value;
+    console.log(data.value);
+    useHead({
+      title: data.value,
+    })
+  } else {
+    appname.value = "Unknown";
+  }
+} catch (error) {
+  console.error('Failed to fetch:', error);
+}
+</script>
